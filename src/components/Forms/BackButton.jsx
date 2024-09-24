3d5367d4a8b933e71baffe9dff7cd3ce77ab272f
Mederic Fauvernier <mederic.fauvernier@gmail.com>
2024-09-24 16:05:01 +0200

    finished signin features

--- src/components/Forms/BackButton.jsx
+++ src/components/Forms/BackButton.jsx
@@ -1,17 +1,29 @@
 import { IoIosArrowBack } from "react-icons/io";
 
+import { useNavigate } from "react-router-dom";
+
 import "./BackButton.css";
 
 const BackButton = ({ step, setStep }) => {
+  const navigate = useNavigate();
   return (
     <>
-      {step > 1 && (
+      {step > 1 ? (
         <button
           // This function get back to the previous step
           onClick={() => {
             if (step) {
               setStep(step - 1);
             }
+          }}
+          className="back-button"
+        >
+          <IoIosArrowBack />
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            navigate("/");
           }}
           className="back-button"
         >
