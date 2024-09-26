588532a4b1a27b6e25877fed06b1d4f32ffad178
takonguina <tommy.akonguina@yahoo.fr>
2024-09-26 11:30:33 +0200

    add condition to login button header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Header = () => {
+  const { token } = useContext(AuthContext);
+
   return (
     <div className="header-container">
       <div className="header-content">
         <Link to="/" style={{ textDecoration: "none" }}>
           <span className="veni-logo">veni</span>
         </Link>
-        <Link
-          to="/signUp"
-          style={{ textDecoration: "none", color: "white", marginTop: "5px" }}
-        >
-          <span>S'inscrire / Se Connecter</span>
-        </Link>
+        {!token && (
+          <Link
+            to="/signUp"
+            style={{ textDecoration: "none", color: "white", marginTop: "5px" }}
+          >
+            <span>S'inscrire / Se Connecter</span>
+          </Link>
+        )}
       </div>
     </div>
   );
