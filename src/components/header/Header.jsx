aa62d93043db36e7a34402b6dc0abcc44a8e54fc
takonguina <tommy.akonguina@yahoo.fr>
2024-09-26 11:26:48 +0200

    header redirect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header-container">
       <div className="header-content">
-        <span className="veni-logo">veni</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="veni-logo">veni</span>
+        </Link>
+        <Link
+          to="/signUp"
+          style={{ textDecoration: "none", color: "white", marginTop: "5px" }}
+        >
+          <span>S'inscrire / Se Connecter</span>
+        </Link>
       </div>
     </div>
   );
